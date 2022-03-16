73c3901c7476987eb99c2cce1bf91351391a820c
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-03-16 19:10:43 +0530

    practise react

---
+++ react-bootcamp/portfolio/src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import Projects from "./Projects";
+import SocialProfiles from "./SocialProfiles";
+import Profile from "../assets/photograph_abhijeet.JPG";
+import Title from "./Title";
+
+class App extends Component {
+  state = { displayBio: false };
+
+  toggleDisplayBio = () => {
+    this.setState({ displayBio: !this.state.displayBio });
+  };
+  render() {
+    return (
+      <div>
+        <img src={Profile} alt="profile" className="profile" />
+        <h1>Hello!</h1>
+        <p>I Am Abhijeet.</p>
+        {/* {this.state.displayBio ? <Title /> : null} */}
+        <Title />
+        <p>I look forward to learn react.</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Pune.</p>
+            <p>I code everyday and I like JS</p>
+            <p>React JS is awesome</p>
+            <p>Besides codeing, I like music</p>
+            <button onClick={this.toggleDisplayBio}>Show less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read more</button>
+          </div>
+        )}
+        <hr />
+        <Projects />
+        <hr />
+        <SocialProfiles />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ react-bootcamp/portfolio/src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import emailIcon from "../assets/email_icon.png";
+import githubIcon from "../assets/github_icon.png";
+import linkdedinIcon from "../assets/linkedin_icon.png";
+import twitterIcon from "../assets/twitter_icon.png";
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: "mailto: abhijeetsakhalkar1305@gmail.com",
+    image: emailIcon,
+  },
+  {
+    id: 2,
+    link: "https://github.com/abj1305",
+    image: githubIcon,
+  },
+  {
+    id: 3,
+    link: "https://linkedin.com",
+    image: linkdedinIcon,
+  },
+  {
+    id: 4,
+    link: "https://twitter.com",
+    image: twitterIcon,
+  },
+];
+
+export default SOCIAL_PROFILES;
