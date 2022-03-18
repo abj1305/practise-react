faf1122a5162bea564c2214454e76f19960749a8
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-03-18 17:59:14 +0530

    user input in music master

--- react-bootcamp/music-master/src/components/App.js
+++ react-bootcamp/music-master/src/components/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  state = { artistQuery: "" };
+
+  updateArtistQuery = (event) => {
+    console.log("event.target.value", event.target.value); // gets updated value from input search bar
+    this.setState({ artistQuery: event.target.value });
+  };
+
+  searchArtist = () => {
+    console.log("this.state", this.state);
+  };
+
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      this.searchArtist();
+    }
+  };
+
   render() {
-    return <div>React App</div>;
+    return (
+      <div>
+        <h2>Music Master</h2>
+        <input
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          placeholder="search for an artist"
+        />
+        <button onClick={this.searchArtist}>Search</button>
+      </div>
+    );
   }
 }
 export default App;
