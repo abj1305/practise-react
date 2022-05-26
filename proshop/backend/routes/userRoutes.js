655136d9924cbf82607f0213a884e6bcf0eed873
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2022-05-26 12:27:27 +0530

    profile screen

--- proshop/backend/routes/userRoutes.js
+++ proshop/backend/routes/userRoutes.js
@@ -3,11 +3,15 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.route("/").post(registerUser);
 router.route("/login").post(authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
