34ad7148a7f11d1bb4d07f823b0863768e313666
Abhijeet Sakhalkar <abhijeet.sakhalkar@wipro.com>
2023-04-18 19:11:35 +0530

    put to git

--- practise-exercises/basic-counter-app/src/App.js
+++ practise-exercises/basic-counter-app/src/App.js
@@ -1,40 +1,54 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import TagManager from "react-gtm-module";
+const tagManagerArgs = {
+  gtmId: "GTM-PWN8VJZ",
+};
+TagManager.initialize(tagManagerArgs);
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      counter:0
-    }
+      counter: 0,
+    };
   }
 
   increment = () => {
-    this.setState({counter : this.state.counter + 1});
+    this.setState({ counter: this.state.counter + 1 });
+    window.dataLayer.push({
+      event: "{event-name}",
+      label: "{some-more-information}",
+    });
   };
 
   decrement = () => {
-    this.setState({counter : this.state.counter - 1});
-  }
+    this.setState({ counter: this.state.counter - 1 });
+    window.dataLayer.push({
+      event: "decrement",
+    });
+  };
 
-  render () {
+  render() {
     return (
-      <div className='App'>
-        <div className='row'>
-            <div>
-                <button onClick={this.increment} className='counter'>+</button>
-            </div>
-            
-            <div>
-                <button onClick={this.decrement} className='counter'>-</button>
-            </div>
-        </div>      
+      <div className="App">
+        <div className="row">
+          <div>
+            <button onClick={this.increment} className="counter">
+              +
+            </button>
+          </div>
+
+          <div>
+            <button onClick={this.decrement} className="counter">
+              -
+            </button>
+          </div>
+        </div>
         <h2>Counter: {this.state.counter}</h2>
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
